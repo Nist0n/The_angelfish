e5ea3e09f8f832b1db2839dbbec1192e5ed294ae
Niston <vadimpavlov@bk.ru>
2024-04-28 01:51:23 +0300

    camera

--- Assets/Scripts/Camera/StartGameCamera.cs
+++ Assets/Scripts/Camera/StartGameCamera.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class StartGameCamera : MonoBehaviour
 {
     [SerializeField] private GameObject point;
+    [SerializeField] private GameObject gameUI;
 
     private void Update()
     {
         Vector3 newPos = Vector3.MoveTowards(transform.position, point.transform.position, 20 * Time.deltaTime);
         transform.position = newPos;
+
+        if (Vector3.Distance(transform.position, point.transform.position) <= 5f)
+        {
+            gameUI.SetActive(true);
+        }
     }
 }
